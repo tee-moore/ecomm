5d352ae0ceeae2f099856cbeb82a7becce2bc3f6
tee-moore <2teemoore@gmail.com>
2018-12-07 18:15:39 +0200

    add settings file

---
+++ config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'imagefolder' => [
+        'root' => 'images',
+        'default' => 'images/default/',
+        'products' => 'images/products/',
+        'users' => 'images/users/',
+    ],
+
+];
