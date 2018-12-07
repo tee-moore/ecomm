e551479e76a1b834fdc3a64fe06f6a603f57da39
tee-moore <2teemoore@gmail.com>
2018-12-07 18:06:42 +0200

    install Form Html Service

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
@@ -228,6 +229,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
