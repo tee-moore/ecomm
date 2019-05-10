c574dbba3732401cad1ad42cd29ea25c726fb8b8
tee-moore <2teemoore@gmail.com>
2019-05-10 17:30:11 +0300

    Moved Relation::morphMap to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Brand;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Variation;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +28,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // for poly morph relation Media tables
+        Relation::morphMap([
+            0 => Product::class,
+            1 => Variation::class,
+            2 => Brand::class,
+            3 => User::class,
+        ]);
     }
 }
