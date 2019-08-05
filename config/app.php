14f9ba8bc1e4b94b08813ac414c2f45c359688a8
tee-moore <2teemoore@gmail.com>
2019-08-05 17:47:23 +0300

    Refactored helpers

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         /*
-         * ecomm
+         * Ecomm Service Providers...
          */
         App\Providers\HelperServiceProvider::class,
     ],
